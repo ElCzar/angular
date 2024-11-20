7b6cdd08296292534948eed8676a6d5dad63d404
elczar <cesarandresolarte@gmail.com>
2024-11-19 19:36:36 -0500

    feat: parcial crud contrato

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ContratosVerComponent } from './components/contratos-ver/contratos-ver.component';
+import { ContratosCuComponent } from './components/contratos-cu/contratos-cu.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/ver', pathMatch: 'full' },
+  { path: 'ver', component: ContratosVerComponent },
+  { path: 'cu', component: ContratosCuComponent }
+];
